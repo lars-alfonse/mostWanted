7ca734b5452d4b17c6dbeeedc3aafa7475bc1614
lars-alfonse <lars.alfonse@gmail.com>
2017-07-25 13:34:04 -0500

    cleaned up master code function spacing and comments

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 "use strict"
-/*
-Build all of your functions for displaying and gathering information below (GUI).
-*/
 
-// app is the function called to start the entire application
 function app(people){
   var person;
   var searchResult;
@@ -15,46 +11,36 @@ function app(people){
     case 'no':
         searchResult = searchByTrait(people);
         person = searchResult;
-
     break;
     default:
-    app(people); // restart app
+    app(people);
     break;
-
   }
   mainMenu(person, people);
-
 }
-
-// Menu function to call once you find who you are looking for
 function mainMenu(person, people){
-
-  /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
-
-  if(!person){
-    alert("Could not find that individual.");
-    return app(people); // restart
-  }
-
-  var displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
-
-  switch(displayOption){
-    case "info":
-        getPersonInfo(person);
-    break;
-    case "family":
-        getPersonFamily(person, people);
-    break;
-    case "descendants":
-        getdescendants(person, people);
-    break;
-    case "restart":
-    app(people); // restart
-    break;
-    case "quit":
-    return; // stop execution
-    default:
-    return mainMenu(person, people); // ask again
+    if(!person){
+        alert("Could not find that individual.");
+        return app(people);
+    }
+    var displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
+    switch(displayOption){
+        case "info":
+            getPersonInfo(person);
+        break;
+        case "family":
+            getPersonFamily(person, people);
+        break;
+        case "descendants":
+            getdescendants(person, people);
+        break;
+        case "restart":
+            app(people);
+        break;
+        case "quit":
+            return;
+        default:
+            return mainMenu(person, people);
   }
 }
 
@@ -203,28 +189,24 @@ function displayPeople(people){
   }).join("\n"));
 }
 
-function displayPerson(person){
+/*function displayPerson(person){
   // print all of the information about a person:
   // height, weight, age, name, occupation, eye color.
   var personInfo = "First Name: " + person.firstName + "\n";
   personInfo += "Last Name: " + person.lastName + "\n";
   // TODO: finish getting the rest of the information to display
   alert(personInfo);
-}
-
-// function that prompts and validates user input
+}*/
 function promptFor(question, valid){
   do{
     var response = prompt(question).trim();
   } while(!response || !valid(response));
   return response;
 }
-
 // helper function to pass into promptFor to validate yes/no answers
 function yesNo(input){
   return input.toLowerCase() == "yes" || input.toLowerCase() == "no";
 }
-
 // helper function to pass in as default promptFor validation
 function chars(input){
   return true; // default validation only
